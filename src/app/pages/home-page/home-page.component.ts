86cd7cf3124133d8b083ff8780affd99151b2d2e
GabrielLouka <89856380+GabrielLouka@users.noreply.github.com>
2025-03-20 18:41:17 -0400

    intro component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DataViz1Component } from '../../components/data-viz1/data-viz1.component';
+import { IntroductionSectionComponent } from "../../components/introduction-section/introduction-section.component";
 
 @Component({
     selector: 'app-home-page',
-    imports: [DataViz1Component],
+    imports: [DataViz1Component, IntroductionSectionComponent],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
 })
