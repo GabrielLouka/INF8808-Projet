ba88425b59da5b290fb7dc616c61f7c77c34eab5
Steven Ho <steven.ho01@outlook.com>
2025-03-20 15:12:39 -0400

    Initialized data viz 1

---
+++ src/app/components/data-viz1/data-viz1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-data-viz1',
+    imports: [],
+    templateUrl: './data-viz1.component.html',
+    styleUrl: './data-viz1.component.scss',
+})
+export class DataViz1Component {}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DataViz1Component } from '../../components/data-viz1/data-viz1.component';
 
 @Component({
     selector: 'app-home-page',
-    imports: [],
+    imports: [DataViz1Component],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
 })
