fda308352e35c089b105f2461c01b22865edd7cc
Steven Ho <steven.ho01@outlook.com>
2025-03-25 20:39:38 -0400

    Moved categorizeTargetTypeData method to data service

--- src/app/components/visualizations/data-viz1/data-viz1.component.ts
+++ src/app/components/visualizations/data-viz1/data-viz1.component.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
-import {
-    TARGET_CATEGORY_MAPPING,
-    TargetCategory,
-} from '../../../models/category';
-import { CountEntry, DataField, YearEntry } from '../../../models/data';
+import { TargetCategory } from '../../../models/category';
+import { DataField, YearEntry } from '../../../models/data';
 import { DataService } from '../../../services/data/data.service';
 import { LoaderComponent } from '../../loader/loader.component';
 
@@ -44,7 +41,7 @@ export class DataViz1Component implements OnInit {
                 );
 
                 const targetTypeCategories =
-                    this.categorizeTargetTypeData(targetTypeData);
+                    this.dataService.categorizeTargetTypeData(targetTypeData);
 
                 this.createStackedAreaChart(targetTypeCategories);
                 this.isLoading = false;
@@ -234,42 +231,5 @@ export class DataViz1Component implements OnInit {
             .style('text-anchor', 'end')
             .style('font-size', '14px')
             .text((d) => d);
-    }
-
-    private categorizeTargetTypeData(yearlyData: YearEntry[]): YearEntry[] {
-        const categorizedData = yearlyData.map((yearEntry: YearEntry) => {
-            const { year, total, counts } = yearEntry;
-
-            const categoryCountData: CountEntry[] = [
-                { category: TargetCategory.GovernmentAndSecurity, count: 0 },
-                { category: TargetCategory.PrivateSectorAndMedia, count: 0 },
-                {
-                    category: TargetCategory.InfrastructureAndTransport,
-                    count: 0,
-                },
-                {
-                    category: TargetCategory.CiviliansAndSocialInstitutions,
-                    count: 0,
-                },
-                { category: TargetCategory.Others, count: 0 },
-            ];
-
-            counts.forEach((targetTypeCountEntry: CountEntry) => {
-                const category =
-                    TARGET_CATEGORY_MAPPING[targetTypeCountEntry['field']] ||
-                    'Unknown';
-
-                const categoryCountEntry = categoryCountData.find(
-                    (categoryCountEntry: CountEntry) =>
-                        categoryCountEntry['category'] === category
-                );
-
-                categoryCountEntry!.count += targetTypeCountEntry.count;
-            });
-
-            return { year, total, counts: categoryCountData };
-        });
-
-        return categorizedData;
     }
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as d3 from 'd3';
 import { BehaviorSubject } from 'rxjs';
+import { TARGET_CATEGORY_MAPPING, TargetCategory } from '../../models/category';
 import { CountEntry, DataField, YearEntry } from '../../models/data';
 
 @Injectable({
@@ -24,7 +25,7 @@ export class DataService {
             });
     }
 
-    // Pour data viz 1 et 2
+    // Viz 1 et 2
     groupDataByYear(
         data: d3.DSVRowArray<string>,
         fieldName: DataField
@@ -67,5 +68,43 @@ export class DataService {
         }, []);
 
         return groupedData;
+    }
+
+    // Viz 1
+    categorizeTargetTypeData(yearlyData: YearEntry[]): YearEntry[] {
+        const categorizedData = yearlyData.map((yearEntry: YearEntry) => {
+            const { year, total, counts } = yearEntry;
+
+            const categoryCountData: CountEntry[] = [
+                { category: TargetCategory.GovernmentAndSecurity, count: 0 },
+                { category: TargetCategory.PrivateSectorAndMedia, count: 0 },
+                {
+                    category: TargetCategory.InfrastructureAndTransport,
+                    count: 0,
+                },
+                {
+                    category: TargetCategory.CiviliansAndSocialInstitutions,
+                    count: 0,
+                },
+                { category: TargetCategory.Others, count: 0 },
+            ];
+
+            counts.forEach((targetTypeCountEntry: CountEntry) => {
+                const category =
+                    TARGET_CATEGORY_MAPPING[targetTypeCountEntry['field']] ||
+                    'Unknown';
+
+                const categoryCountEntry = categoryCountData.find(
+                    (categoryCountEntry: CountEntry) =>
+                        categoryCountEntry['category'] === category
+                );
+
+                categoryCountEntry!.count += targetTypeCountEntry.count;
+            });
+
+            return { year, total, counts: categoryCountData };
+        });
+
+        return categorizedData;
     }
 }
